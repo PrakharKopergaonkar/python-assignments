abec3178f19db4585222a928c52dbbf506911451
prakhark <42354716+PrakharKopergaonkar@users.noreply.github.com>
2020-01-21 12:49:58 +0530

    Update str_sum_average.py

--- module3/str_sum_average.py
+++ module3/str_sum_average.py
@@ -1,14 +1,13 @@
 string = "English = 78 Science = 83 math = 68 history = 65"
 sum=0
-list1 = ['0', '1', '2', '3', '4', '5', '6', '7','8', '9']
+def isint(i):
+    try:
+        num = int(i)
+    except ValueError:
+        return False
+    return True
 for i in string.split(" "):
-    integer = ""
-    for j in i:
-        if(j in list1):
-            integer+=j
-        else:
-            integer=""
-            break
-    if(integer!=""):
-        sum+=int(integer)
+    if(isint(i)):
+        sum+=int(i)
 print("sum is: {} and avg is: {}".format(sum, sum/4))
+
