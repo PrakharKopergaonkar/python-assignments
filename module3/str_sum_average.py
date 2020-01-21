8dd9159db67172eb0e7655b3c25fded67fd07568
prakhark <42354716+PrakharKopergaonkar@users.noreply.github.com>
2020-01-21 12:57:59 +0530

    Update str_sum_average.py

--- module3/str_sum_average.py
+++ module3/str_sum_average.py
@@ -1,11 +1,11 @@
-string = "English = 78 Science = 83 math = 68 history = 65"
-sum=0
 def isint(i):
     try:
         num = int(i)
     except ValueError:
         return False
     return True
+string = "English = 78 Science = 83 math = 68 history = 65"
+sum=0
 for i in string.split(" "):
     if(isint(i)):
         sum+=int(i)
