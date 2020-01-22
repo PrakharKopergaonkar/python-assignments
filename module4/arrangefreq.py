2077c662d7a12796c2fe84587ade2d3b96fe162a
prakhark <42354716+PrakharKopergaonkar@users.noreply.github.com>
2020-01-22 21:12:53 +0530

    Update arrangefreq.py

--- module4/arrangefreq.py
+++ module4/arrangefreq.py
@@ -1,19 +1,24 @@
-def arrangefreq(list, size):
+def arrangefreq(list1, size):
+    answer_list = []
     #calculate frequency
     freq = dict()
-    for i in list:
+    for i in list1:
         if i in freq.keys():
              freq[i]+=1
         else:
             freq[i]=1
-    #sorting of list if freq[i] < freq[j] -> swap or if freq[i]=freq[j] then if i>j -> swap
-    for i in range(0, size-1):
-        for j in range(i+1, size):
-            if(freq[list[i]] < freq[list[j]]):
-                list[i] , list[j] = list[j], list[i]
-            if((freq[list[i]]==freq[list[j]]) and (list[i]>list[j])):
-                list[i],list[j] = list[j], list[i]
-    print(list)
+     #convert dictonary into list of tuples of values and its frequency 
+    freq = list(freq.items())
+    #sort the list by frequency, if two values have same frequency(higher frequency comes first) they are sorted by value(smaller values comes first)
+    #printing of elements is done in the same for loop
+    for i in range(0, len(freq)-1):
+        for j in range(i+1,len(freq)):
+            if((freq[i][1] < freq[j][1]) or ((freq[i][1] == freq[j][1]) and freq[i][0]>freq[j][0])):
+                freq[i],freq[j]=freq[j],freq[i]
+        print(str(freq[i][0])*freq[i][1],end="")
+        if(i==len(freq)-2):
+            print(str(freq[i+1][0])*freq[i+1][1],end="")
+#main function
 size = int(input("enter size of elements"))
-list = [int(input()) for i in range(0,size)]
-arrangefreq(list, size)
+list1 = [int(input()) for i in range(0,size)]
+arrangefreq(list1, size)
